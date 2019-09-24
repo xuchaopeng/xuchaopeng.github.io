022a655db0f8d4bd7d64981c33a6b44e2514a6cf
xcp <m18770074875_1@163.com>
2019-09-24 09:23:26 +0800

    Create adplus5840796.js

---
+++ static/js/adplus5840796.js
@@ -0,0 +1,38 @@
+__baidu_spp_callback_5840796([
+    {
+      materialSerial: 'w0923', //物料编号
+      materialName: '百度直客投放', //物料名称
+      state: '1', //投放状态  1启用  -1禁用
+      priority: '4', //优先级权重 1 2 3 4 4为最大
+      shieldregion: '北京&南昌&江苏', //屏蔽地域 未屏蔽则显示全地域，存在则前端屏蔽这些地域，其中all代表屏蔽全地域
+      startDate: '2019/01/28 00:00:00', //开始时间（阳历）2019/01/28 00:00:00
+      overDate: '2019/10/28 00:00:00', //结束时间
+      deliv: {
+        deliv_id: '',
+        deliv_des: {
+          _html: {
+            mimeType: 'plain/html', //投放物料代码类别,暂且默认写死plain/html
+            content: '< script >asyncuid("u3578958");asyncuid("u3578958");asyncuid("u3578958");< /script >'
+          }
+        }
+      }
+    },
+    {
+      materialSerial: 'w0924', //物料编号
+      materialName: '百度直客投放', //物料名称
+      state: '1', //投放状态  1启用  -1禁用
+      priority: '3', //优先级权重 1 2 3 4 4为最大
+      shieldregion: '上海&北京&南昌&江苏', //屏蔽地域 未屏蔽则显示全地域，存在则前端屏蔽这些地域，其中all代表屏蔽全地域
+      startDate: '2019/01/28 00:00:00', //开始时间（阳历）2019/01/28 00:00:00
+      overDate: '2019/10/28 00:00:00', //结束时间
+      deliv: {
+        deliv_id: '',
+        deliv_des: {
+          _html: {
+            mimeType: 'plain/html', //投放物料代码类别,暂且默认写死plain/html
+            content: '<script>asyncuid("u3578958");asyncuid("u3578958");asyncuid("u3578958");</script>'
+          }
+        }
+      }
+    }
+  ]);
