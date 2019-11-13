effd40010f4997ec7eca8a0ff97281279d2d4bbd
xcp <m18770074875_1@163.com>
2019-11-13 14:48:33 +0800

    Create d0000163.js

---
+++ static/js/d0000163.js
@@ -0,0 +1,55 @@
+__spp_callback_d0000163({
+  ctime: "2019-11-04 22:13:17",
+  data: [
+    {
+      planId: "177",
+      planName: "嵩恒_头条_PC_新闻页面_all_右1-谷歌",
+      state: "1",
+      priority: "4",
+      shieldregion: "",
+      limit_showtimes: "1|2",
+      date: [
+        {
+          begin_date: "2019/11/01",
+          end_date: "9999/12/31",
+          begin_hour: "00:00",
+          end_hour: "23:59"
+        }
+      ],
+      material: {
+        material_id: "177",
+        material_des: {
+          _html: {
+            type: "html",
+            content: "console.log('我被show了')"
+          }
+        }
+      }
+    },
+    {
+      planId: "219",
+      planName: "嵩恒_头条_PC_新闻页面_all_右1-dsp21",
+      state: "1",
+      priority: "2",
+      shieldregion: "black|北京市&上海市&杭州市&淄博市",
+      date: [
+        {
+          begin_date: "2019/11/04",
+          end_date: "2019/11/04",
+          begin_hour: "00:00",
+          end_hour: "23:59"
+        }
+      ],
+      material: {
+        material_id: "186",
+        material_des: {
+          _html: {
+            type: "html",
+            content:
+              "//右1\nNY_NOVEL58DH_FN({\n showType: 'dw', // 广告类型 commondw/dw/list\n type: 'big', // 类型 大图/4图 big/group\n title: [''], // 标题 没有可不填\n img: ['//imgmini.eastday.com/activityImg/20191104/20191104140335.gif'], // 图片地址\n url: 'http://vba27.vbaaz.top/', // 跳转地址\n pos: 'right1', // 广告位置标识\n ssp: 'd0000163', // sspid\n union: 'dsp21' // 上报类型\n});"
+          }
+        }
+      }
+    }
+  ]
+});
